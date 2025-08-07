44f5a01ec5b2d23fc10bcf4508df7276a08051ce
rithikrajkumar <rithik.rajkumar@icodelabs.co>
2025-08-07 22:22:45 +0530

    Refactor UI components and styles for improved consistency and functionality
    - Updated button styles and variants for better usability
    - Adjusted input and textarea components for consistent height and styling
    - Enhanced progress component with clearer visual indicators
    - Modified global CSS variables for better design alignment
    - Added new UI elements (Button, Checkbox, Textarea) to the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-300">Hello world</p>
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant={"elevated"}>Click Me</Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="I am text area" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
